90dc440abfb8ee7c95df8ed6a3a3f977f3405b69
Mariano138 <agueromariano138@gmail.com>
2024-10-11 16:47:24 -0300

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { Formik } from "formik";
+import * as Yup from "yup";
+
+const loginSchema = Yup.object({
+  email: Yup.string().email("Email invalido").required("required"),
+  password: Yup.string()
+    .required()
+    .min(8, "La contraseña debe tener al menos 8 caracteres"),
+});
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        validationSchema={loginSchema}
+        onSubmit={(values) => console.log(values)}
+      >
+        {({
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          values,
+          errors,
+          touched,
+        }) => (
+          <View>
+            <Text>Email:</Text>
+            <TextInput
+              onChangeText={handleChange("email")}
+              onBlur={handleBlur("email")}
+              value={values.email}
+              keyboardType="email adress"
+            />
+            {touched.email && errors.email && <Text>{errors.email}</Text>}
+
+            <Text>Password:</Text>
+            <TextInput
+              onChangeText={handleChange("password")}
+              onBlur={handleBlur("password")}
+              value={values.password}
+              secureTextEntry
+            />
+            {touched.password && errors.password && (
+              <Text>{errors.password}</Text>
+            )}
+
+            <Button title="submit" onPress={handleSubmit} />
+          </View>
+        )}
+      </Formik>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +59,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
